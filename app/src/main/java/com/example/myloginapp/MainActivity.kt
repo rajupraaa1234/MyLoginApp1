3359c8dc68d8c97c64d09a410951fe181cb68e9e
Raju kumar <rajupraaa1234@gmail.com>
2021-07-15 03:23:41 -0700

    initial commit

---
+++ app/src/main/java/com/example/myloginapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myloginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var etname : EditText;   // declaration
+    lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var btn_new_activity = findViewById(R.id.login) as Button
+        etname = findViewById(R.id.name);
+        textView = findViewById(R.id.mytxt)
+        btn_new_activity.setOnClickListener {
+            var name = etname.text.toString()
+            textView.setText(name)
+            Toast.makeText(this," Hii " + name,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
