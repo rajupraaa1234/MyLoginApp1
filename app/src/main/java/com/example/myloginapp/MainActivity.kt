0acefd91a2655cc1abfd6ad31c428c0ff85839c5
Raju kumar <rajupraaa1234@gmail.com>
2021-07-16 11:27:34 -0700

    clickhandler

--- app/src/main/java/com/example/myloginapp/MainActivity.kt
+++ app/src/main/java/com/example/myloginapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myloginapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -20,24 +21,57 @@ class MainActivity : AppCompatActivity() {
         var home_btn = findViewById(R.id.home) as Button
         etname = findViewById(R.id.name);
         textView = findViewById(R.id.mytxt)
-        home_btn.setOnClickListener {
-            var HomeIntent : Intent
-            HomeIntent =Intent(this,HomeActivity::class.java)
-            HomeIntent.putExtra("MyKey","Hii Raju")
-            startActivity(HomeIntent)
-
-        }
-
-        btn_new_activity.setOnClickListener {
-            var name = etname.text.toString()
-            textView.setText(name)
-            var sum = add(10,20)
-            Toast.makeText(this," Hii " + name+ sum,Toast.LENGTH_SHORT).show();
-        }
+//        home_btn.setOnClickListener {
+//            var HomeIntent : Intent
+//            HomeIntent =Intent(this,HomeActivity::class.java)
+//            HomeIntent.putExtra("MyKey","Hii Raju")
+//            startActivity(HomeIntent)
+//
+//        }
+//
+//        btn_new_activity.setOnClickListener {
+//            var name = etname.text.toString()
+//            textView.setText(name)
+//            var sum = add(10,20)
+//            Toast.makeText(this," Hii " + name+ sum,Toast.LENGTH_SHORT).show();
+//        }
     }
 
     fun add(a : Int,b : Int) : Int{
         return a+b
+    }
+
+    fun ClickHandler(view: View){
+        when(view.id){
+            R.id.login->{loginbtn()}
+            R.id.home->{goHome()}
+            R.id.google->{openGoogle()}
+            R.id.dail->{openDail()}
+        }
+    }
+
+    private fun openDail() {
+        var gIntent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:123456789"))
+        startActivity(gIntent)
+    }
+
+    private fun openGoogle() {
+        var gIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.in/"))
+        startActivity(gIntent)
+    }
+
+    private fun goHome() {
+        var HomeIntent : Intent
+        HomeIntent =Intent(this,HomeActivity::class.java)
+        HomeIntent.putExtra("MyKey","Hii Raju")
+        startActivity(HomeIntent)
+    }
+
+    private fun loginbtn() {
+        var name = etname.text.toString()
+        textView.setText(name)
+        var sum = add(10,20)
+        Toast.makeText(this," Hay " + name+ sum,Toast.LENGTH_SHORT).show();
     }
 
 }
