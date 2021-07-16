8402342a603f35c2f651957c9d36bbbb538f03b2
Raju kumar <rajupraaa1234@gmail.com>
2021-07-16 09:58:18 -0700

    add new activity_home.xml

--- app/src/main/java/com/example/myloginapp/MainActivity.kt
+++ app/src/main/java/com/example/myloginapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myloginapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,8 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btn_new_activity = findViewById(R.id.login) as Button
+        var home_btn = findViewById(R.id.home) as Button
         etname = findViewById(R.id.name);
         textView = findViewById(R.id.mytxt)
+        home_btn.setOnClickListener {
+            var HomeIntent : Intent
+            HomeIntent =Intent(this,HomeActivity::class.java)
+            startActivity(HomeIntent)
+
+        }
+
         btn_new_activity.setOnClickListener {
             var name = etname.text.toString()
             textView.setText(name)
