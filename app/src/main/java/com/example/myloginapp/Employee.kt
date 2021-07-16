22e470f3cb5e536ed028e703c898d7bca1a15721
Raju kumar <rajupraaa1234@gmail.com>
2021-07-16 09:37:58 -0700

    add test file

---
+++ app/src/main/java/com/example/myloginapp/Employee.kt
@@ -0,0 +1,22 @@
+package com.example.myloginapp
+
+class Employee {
+//    var name: String? = null
+//    var id: Int
+//    var Address: String? = null
+//
+//    constructor(name: String?, id: Int, address: String?) {
+//        this.name = name
+//        this.id = id
+//        Address = address
+//    }
+//
+//    constructor(id: Int) {
+//        this.id = id
+//    }
+}
+
+fun main() {
+    print("Hello ")
+    print("world!")
+}
---
+++ app/src/main/java/com/example/myloginapp/Student.kt
@@ -0,0 +1,7 @@
+package com.example.myloginapp
+
+class Student {
+    var name: String? = null
+    var id = 0
+    var address: String? = null
+}
