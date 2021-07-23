b06a2b66fbfb145372ca4643ebbcf9c9cc03493c
rajukumar <rajupraaa1234@gmail.com>
2021-07-23 13:07:12 +0530

    added bound services

---
+++ app/src/main/java/com/example/myloginapp/MyServices/MyBoundServices.kt
@@ -0,0 +1,32 @@
+package com.example.myloginapp.MyServices
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import java.util.*
+
+class MyBoundServices : Service() {
+    // Binder given to clients
+    private val binder = LocalBinder()
+
+    // Random number generator
+    private val mGenerator = Random()
+
+    /** method for clients  */
+    val randomNumber: Int
+        get() = mGenerator.nextInt(100)
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyBoundServices = this@MyBoundServices
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+}
--- app/src/main/java/com/example/myloginapp/MyServices/MyServicesActivity.kt
+++ app/src/main/java/com/example/myloginapp/MyServices/MyServicesActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myloginapp.MyServices
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import android.widget.Toast
 import com.example.myloginapp.R
 
 class MyServicesActivity : AppCompatActivity() {
+
+    private lateinit var mService: MyBoundServices
+    private var mBound: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_services)
@@ -22,7 +31,47 @@ class MyServicesActivity : AppCompatActivity() {
                 var serviceIntent = Intent(this,MyService::class.java)
                 stopService(serviceIntent)
             }
+            R.id.boundservice->{
+                if (mBound) {
+                    // Call a method from the LocalService.
+                    // However, if this call were something that might hang, then this request should
+                    // occur in a separate thread to avoid slowing down the activity performance.
+                    val num: Int = mService.randomNumber
+                    Toast.makeText(this, "number: $num", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
+
+
+    /** Defines callbacks for service binding, passed to bindService()  */
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as MyBoundServices.LocalBinder
+            mService = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Bind to LocalService
+        Intent(this, MyBoundServices::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
+    }
+
 
 }
