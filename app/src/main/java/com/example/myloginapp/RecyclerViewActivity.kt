ef4dbeb2965e148b4f8e8958b54b9e9d9e8a96f9
rajukumar <rajupraaa1234@gmail.com>
2021-07-27 23:30:30 +0530

    added swipe delete function

--- app/src/main/java/com/example/myloginapp/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/myloginapp/RecyclerViewActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myloginapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myloginapp.Dialog.ExampleDialog
 import com.example.myloginapp.interfacePackage.OnClickListner
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class RecyclerViewActivity : AppCompatActivity(), OnClickListner,ExampleDialog.ExampleDialogListener{
 
@@ -42,11 +45,57 @@ class RecyclerViewActivity : AppCompatActivity(), OnClickListner,ExampleDialog.E
             exampleDialog.show(getSupportFragmentManager(), "example dialog");
         })
 
+        initRecyclerView()
+        swipeDelte()
+    }
+
+    fun initRecyclerView(){
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = RecAdapter(this,data)
         recyclerView.adapter = adapter
-
     }
+
+    fun swipeDelte(){
+        var str : String
+        val itemTouchHelperCallback =
+            object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    var pos : Int
+                    pos = viewHolder.adapterPosition
+                    when(direction){
+                        ItemTouchHelper.LEFT->{
+                            str = data.get(pos)
+                            data.removeAt(pos)
+                            adapter.notifyItemRemoved(pos)
+                            adapter.notifyDataSetChanged()
+                            Snackbar.make(recyclerView,"Data deleted",Snackbar.LENGTH_LONG)
+                                .setAction("Undo",View.OnClickListener {
+                                    data.add(pos,str)
+                                    adapter.notifyItemInserted(pos)
+                                }).show()
+                            true
+                        }
+                        ItemTouchHelper.RIGHT->{
+                            true
+                        }
+                    }
+                }
+            }
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+
 
     override fun onItemClick(position: Int) {
         Toast.makeText(this,"Hello ${data[position]}",Toast.LENGTH_SHORT).show()
